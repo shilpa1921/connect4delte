643dce9d1995a7fca9b30a765d86897aa1fc2eb0
shilpabbiradar <shilpabiradar4@gmail.com>
2020-03-04 16:20:57 +0100

    Added the 4th Mar Exercise file

---
+++ 4thMarexe.js
@@ -0,0 +1,29 @@
+function sum() {
+    var sumOfArg = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        sumOfArg = sumOfArg + arguments[i];
+    }
+    console.log("sum of the arguments is: " + sumOfArg);
+}
+
+sum(10, 20, 40);
+
+function toCheckNumber(num) {
+    if (num <= 0 || isNaN(NaN) == Number.isNaN(num)) {
+        console.log("ERROR");
+    } else if (num >= 1000000) {
+        console.log(num);
+    } else {
+        num = num * 10;
+        return toCheckNumber(num);
+    }
+}
+
+toCheckNumber(10);
+
+setTimeout(function() {
+    setTimeout(function() {
+        console.log("Goodbye");
+    }, 1500);
+    console.log("Hello!");
+}, 1500);
