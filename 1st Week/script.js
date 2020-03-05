98789c785a4d851081d4602e16210ec757ab83ec
shilpabbiradar <shilpabiradar4@gmail.com>
2020-03-05 12:26:32 +0100

    Made folder

--- 1st Week/script.js
+++ 1st Week/script.js
@@ -31,3 +31,35 @@ function fizzbuzz() {
 
     console.log(typeof 9);
 }
+
+function toCheckNumber(num) {
+    if (num <= 0) {
+        if ((num = Number.isNaN(num))) {
+            console.log("ERROR");
+        }
+    } else if (num >= 1000000) {
+        return num;
+    } else {
+        num = num * 10;
+        return toCheckNumber(num);
+    }
+}
+
+toCheckNumber(0);
+
+num <= 0 || num = Number.isNaN(num);
+
+var totaler = getTotaler();
+
+function getTotaler(num) {
+    var pNum = 0;
+    if (pNum < 0) {
+        return num;
+    } else {
+        return totaler(pNum) + num;
+    }
+    pNum = num;
+}
+
+totaler(2);
+totaler(3);
