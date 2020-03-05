b984ba2c5f992ef6c53521660a8933142b681782
shilpabbiradar <shilpabiradar4@gmail.com>
2020-03-05 10:15:23 +0100

    Added the revised code to 4thMarexe.js file

--- 4thMarexe.js
+++ 4thMarexe.js
@@ -3,27 +3,31 @@ function sum() {
     for (var i = 0; i < arguments.length; i++) {
         sumOfArg = sumOfArg + arguments[i];
     }
-    console.log("sum of the arguments is: " + sumOfArg);
+    return sumOfArg;
 }
 
-sum(10, 20, 40);
+var sumOfNum = sum(10, 20, 40);
+console.log("Sum is " + sumOfNum);
 
 function toCheckNumber(num) {
     if (num <= 0 || isNaN(NaN) == Number.isNaN(num)) {
-        console.log("ERROR");
+        return "ERROR";
     } else if (num >= 1000000) {
-        console.log(num);
+        return num;
     } else {
         num = num * 10;
         return toCheckNumber(num);
     }
 }
 
-toCheckNumber(10);
+var numProduced = toCheckNumber(10);
+console.log("The Number is " + numProduced);
 
-setTimeout(function() {
-    console.log("Hello!");
+function waitThenRun() {
     setTimeout(function() {
-        console.log("Goodbye");
+        console.log("Hello!");
+        setTimeout(function() {
+            console.log("Goodbye!");
+        }, 1500);
     }, 1500);
-}, 1500);
+}
