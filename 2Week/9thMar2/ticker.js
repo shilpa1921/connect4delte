895736d5abd7986cafb92060005e44a40ff991b0
shilpabbiradar <shilpabiradar4@gmail.com>
2020-03-09 17:34:44 +0100

    Ticker Exercise

--- 2Week/9thMar2/ticker.js
+++ 2Week/9thMar2/ticker.js
@@ -1,13 +1,13 @@
 (function() {
     var headlines = document.getElementById("headlines");
 
-    var links = headlines.getElementsByTagName("A");
+    var links = headlines.getElementsByTagName("a");
     var left = headlines.offsetLeft;
 
     function moveHeadlines() {
         left--;
         if (left < -links[0].offsetWidth) {
-            left = left + link[0].offsetWidth;
+            left = left + links[0].offsetWidth;
             links[0].parentNode.appendChild(links[0]);
         }
         headlines.style.left = left + "px";
@@ -15,4 +15,5 @@
     }
 
     moveHeadlines();
+    requestAnimationFrame(moveHeadlines);
 })();
